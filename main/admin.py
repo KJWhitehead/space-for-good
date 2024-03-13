c0b636e9b338630e3c34b07f409bf3a515b0d9eb
KJ Whitehead <kenyon.whitehead@gmail.com>
2024-03-13 11:49:36 +0000

    Add reservation and space models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Space
+from .models import Reservation
 
 # Register your models here.
+admin.site.register(Space)
+admin.site.register(Reservation)
