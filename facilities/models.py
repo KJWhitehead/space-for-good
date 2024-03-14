cf8cdbc593d3ac753fff5933d6d0be1cbeff474f
KJ Whitehead <kenyon.whitehead@gmail.com>
2024-03-14 14:52:03 +0000

    Feat: Initial reservation form

--- facilities/models.py
+++ facilities/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -8,3 +10,40 @@ class Facilities(models.Model):
 
     def __str__(self):
         return self.title
+
+class Space(models.Model):
+
+    TYPES = (
+        ("Meeting Room", "Meeting Room"),
+        ("Open Seating", "Open Seating"),
+    )
+
+    name = models.CharField(max_length=50, null=False, blank=False)
+    max_capacity = models.PositiveIntegerField(
+        default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(12)],
+        null=False, blank=False
+    )
+    space_type = models.CharField(choices=TYPES, max_length=20, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Reservation(models.Model):
+
+    TIMES = (
+        ("09:00-11:00", "09:00-11:00"),
+        ("11:00-13:00", "11:00-13:00"),
+        ("13:00-15:00", "13:00-15:00"),
+        ("15:00-17:00", "15:00-17:00"),
+        ("17:00-19:00", "17:00-19:00"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="main.Reservation.user+")
+    space = models.ForeignKey(Space, on_delete=models.CASCADE)
+    date = models.DateField(null=False, blank=False)
+    time = models.CharField(choices=TIMES, max_length=20, null=False, blank=False)
+
+    def __str__(self):
+        return self.user.username
+
--- facilities/views.py
+++ facilities/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from .models import Facilities
+from .forms import ReservationForm
 
 def facilities_info(request):
     """
     Renders Facilities page
     """
     facilities = Facilities.objects.all().order_by('title').first()
+    reservation_form = ReservationForm()
 
     return render(
         request,
         "facilities/facilities.html",
-        {"facilities": facilities},
+        {"facilities": facilities,
+        "reservation_form": reservation_form},
+        
     )
 
 # Create your views here.
