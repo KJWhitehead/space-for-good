676bb07e8f110a44e783236295138073bdd1fa2f
KJ Whitehead <kenyon.whitehead@gmail.com>
2024-03-15 10:35:54 +0000

    Feat: Increase weight of active page in navbar

--- facilities/forms.py
+++ facilities/forms.py
@@ -4,4 +4,4 @@ from .models import Reservation
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
-        fields = '__all__'
+        fields = 'space', 'date', 'time'
